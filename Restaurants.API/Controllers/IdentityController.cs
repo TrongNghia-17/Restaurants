360cba8a438b2ba6c1dc7d3910b2238427b1c6e6
NoOneTopVN <huynhtrongnghia15n@gmail.com>
2025-01-19 21:39:08 +0700

    Update Custom Claims

--- Restaurants.API/Controllers/IdentityController.cs
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -1,7 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Restaurants.Application.Users.Commands;
+using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Application.Users.Commands.UnassignUserRole;
+using Restaurants.Application.Users.Commands.UpdateUserDetails;
+using Restaurants.Domain.Constants;
 
 namespace Restaurants.API.Controllers;
 
@@ -10,11 +13,26 @@ namespace Restaurants.API.Controllers;
 public class IdentityController(IMediator mediator) : ControllerBase
 {
     [HttpPatch("user")]
-    [Authorize]
+    [AllowAnonymous]
     public async Task<IActionResult> UpdateUserDetails(UpdateUserDetailsCommand command)
     {
         await mediator.Send(command);
+        return NoContent();
+    }
 
+    [HttpPatch("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+    {
+        await mediator.Send(command);
+        return NoContent();
+    }
+
+    [HttpDelete("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> UnassignUserRole(UnassignUserRoleCommand command)
+    {
+        await mediator.Send(command);
         return NoContent();
     }
 }
