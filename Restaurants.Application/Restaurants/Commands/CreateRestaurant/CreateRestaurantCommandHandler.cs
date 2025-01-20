b48dd164785d567d80ef519ebd4ef1b70ed7b7d1
NoOneTopVN <huynhtrongnghia15n@gmail.com>
2025-01-20 20:22:06 +0700

    User resources

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -1,20 +1,26 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Users;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 
 public class CreateRestaurantCommandHandler(
-    ILogger<CreateRestaurantCommandHandler> logger, IMapper mapper, IRestaurantsRepository restaurantsRepository) : IRequestHandler<CreateRestaurantCommand, int>
+    ILogger<CreateRestaurantCommandHandler> logger, IMapper mapper, IRestaurantsRepository restaurantsRepository,
+    IUserContext userContext) : IRequestHandler<CreateRestaurantCommand, int>
 {
     public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Create a new restaurant {@Restaurant}", request);
-        var restaurant = mapper.Map<Restaurant>(request);
-        int id = await restaurantsRepository.Create(restaurant);
+        var currentUser = userContext.GetCurrentUser();
 
+        logger.LogInformation("{UserEmail} [{UserId}] is creating a new restaurant {@Restaurant}", currentUser.Email, currentUser.Id, request);
+
+        var restaurant = mapper.Map<Restaurant>(request);
+        restaurant.OwnerId = currentUser.Id;
+
+        int id = await restaurantsRepository.Create(restaurant);
         return id;
     }
 }
